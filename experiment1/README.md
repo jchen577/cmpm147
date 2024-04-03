6c56a969a364344c5e94e2a7b922f5ae654e2879
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-04-03 12:48:50 -0700

    experiment 1 finished

---
+++ experiment1/README.md
@@ -0,0 +1,7 @@
+<h1>
+  Description
+</h1>
+<h2>
+  This project is a remix of 'bad quests'. The goal of this project is to make interesting randomized statements/phrases that hopefully makes people laugh.
+  The central theme of the project and its text is based on games and gaming tropes.
+</h2>
--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -7,28 +7,51 @@
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
+const fillers = {
+  gamers: ["Gamers","Homies","Fellas that lack sleep","Individuals that need grass","Button mashers","G-fuelers","Monkey wrenches"],
+  first: ["Mountain Dew!","bitches.","seodorant.","sleep!","better games!"],
+  people: ["kindly", "meek", "brave", "wise", "sacred", "cherished", "honored", "forgotten", "apathetic", "mystic", "orca", "帥氣"],
+  item: ["laptop","box","pickaxe","microphone","hope","anxiety","depression","jokes","happiness","homework","mouse","monitor"],
+  num: ["two", "three", "eleven", "so many", "too many", "an unsatisfying number of", "barely any", "an unspecified amount of", "surely a satisfactory number of"],
+  loot1: ["old","busted","OP","sparkly","awe-inspiring","remarkable","flashy","lame","horn-swaggling","eye-opening"],
+  loot2: ["bandwiths"],
+  baddies: ["Wes Modes", "Bob","Josh","that one guy from the dining hall","Einstein","squirrels","rabbits with carrots","Joe"],
+  message: ["summon","discord call","notification","alert","the words coming out my mouth","...o there they go"],
   
-  // define a method
-  myMethod() {
-    // code to run when method is called
+};
+
+const template = `$gamers, listen to my $message!
+
+I'm here to announce that we are in deseperate need of $first If we do not refill immediately, we will be absolutely destroyed by $baddies. You must venture forth at once, taking my $item, and help yourself.
+
+Once you save yourself you will be awarded with $num $loot1 $loot2. So uhhhhhhh good luck with that.
+`;
+
+
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  box.innerText = story;
 }
 
-// let's get this party started - uncomment me
-//main();
+/* global clicker */
+clicker.onclick = generate;
+
+generate();
