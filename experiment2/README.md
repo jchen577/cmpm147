cbeb23b023507665d6b9f9858442f00957bd0e9c
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-04-10 19:24:54 -0700

    experiment 2 finished

---
+++ experiment2/README.md
@@ -0,0 +1,9 @@
+This project offers a generative, living impression of this inspiring image:
+<img src="https://cdn.glitch.global/206f45d0-8e1d-42fa-8991-23752caffcca/landscape.jpg?v=1712775723988"> [source]( https://www.google.com/imgres?q=lake&imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fa%2Fab%2FGentau_Pic_du_Midi_Ossau.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FLake&docid=TnWzHMjZPh4KrM&tbnid=oTLcllfcJX0OBM&vet=12ahUKEwiVspCYq7iFAxXwLkQIHacWBhMQM3oECHQQAA..i&w=4154&h=2717&hcb=2&ved=2ahUKEwiVspCYq7iFAxXwLkQIHacWBhMQM3oECHQQAA)
+
+Relation to inspring image:
+
+- Color: The sky, grass, tree, and stone colors were sampled from the inspiring image using the Photoshop eyedropper tool.
+- Primitives: The trees are rendered with `triangle` primitives. The background is rendered with `rectangle` primitives.
+- Polygons: The background mountains is made with random polygons using `vertex` calls, one colored green and one colored grey.
+- Composition: Different random seed values lead to differently shaped mountains, differently positioned trees, and different sized lakes.
--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,19 +1,24 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Jacky Chen
+// Date: 4/10/2024
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
 
 // Globals
 let myInstance;
 let canvasContainer;
 var centerHorz, centerVert;
+
+let seed = 0;
+
+const skyColor = "#87CEEB";
+const mountColor = "#858290";
+const grassColor = "#138510";
+const lakeColor = "#A9D6D9";
+const lakeColor2 = "#00008B";
+const dryGrassColor = "#e4dc8a";
+const treeColor = "#33330b";
 
 class MyClass {
     constructor(param1, param2) {
@@ -38,7 +43,7 @@ function resizeScreen() {
 function setup() {
   // place our canvas, making it fit our container
   canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(),WEBGL);
   canvas.parent("canvas-container");
   // resize canvas is the page is resized
 
@@ -49,31 +54,85 @@ function setup() {
     resizeScreen();
   });
   resizeScreen();
+  createButton("reimagine").mousePressed(() => seed++);
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
+  randomSeed(seed);
 
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
+  background(100);
   noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+  
+  fill(skyColor);
+  rect(-width/2,-height/5,width,-height/2)
+  
+  fill(mountColor);
+  beginShape();
+  vertex(-width, -height /5);
+  const steps = 10;
+  for (let i = 0; i < steps + 1; i++) {
+    let x = (width/2 * i) / steps;
+    let y;
+    if(i >= 3){
+      y =
+        -height / 5 - (random() * random() * random() * height) /3 - height / 15;
+    }
+    else{
+      y =
+        -height / 5 - (random() * random() * random() * height) /4 - height / 25;
+    }
+    vertex(x, y);
+  }
+  vertex(width, -height / 5);
+  endShape(CLOSE);
+  
+  fill(grassColor);
+  beginShape();
+  vertex(-width, -height /5);
+  for (let i = 0; i < 3; i++) {
+    let x = (width/4 * i) / steps;
+    let y =
+        -height / 5 - (random() * random() * random() * height) /5 - height / 40;
+    vertex(x, y);
+  }
+  vertex(width, -height / 5);
+  endShape(CLOSE);
+  
+  
+  fill(dryGrassColor);
+  rect(-width/2,-height/5,width,height - height/5)
+  
+  push();
+  fill(lakeColor);
+  beginShape();
+  translate(0,30,0);
+  rotateZ(-20);
+  let x = random(70,100);
+  let y =random(100,125);
+  let z = random(50,100);
+  ellipsoid(x,y,z);
+  fill(lakeColor2);
+  ellipsoid(x-10,y-10,z+10);
+  endShape(CLOSE);
+  pop();
+  
+  fill(treeColor);
+  const scrub = mouseX/width;
+  const trees = random(10,20);
+  for (let i = 0; i < trees; i++) {
+    let z = random();
+    let x = -width/2* ((random() + (scrub/50 + millis() / 500000.0) / z) % 1);
+    let s = width/2 / 50 / z;
+    let y = -height/2 / 2 + height / 20 / z;
+    triangle(x, y - s, x - s / 4, y, x + s / 4, y);
+  }
+  
+  for (let i = 0; i < trees; i++) {
+    let z = random();
+    let x = width/2* ((random() + (scrub/50 + millis() / 500000.0) / z) % 1);
+    let s = width/2 / 50 / z;
+    let y = -height/2 / 2 + height / 20 / z;
+    triangle(x, y - s, x - s / 4, y, x + s / 4, y);
+  } 
 }
