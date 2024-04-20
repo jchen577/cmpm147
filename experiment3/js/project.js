b095451d3f2a6b72f383662b04cb2d51504fc55a
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-04-20 01:11:34 -0700

    art done

--- experiment3/js/project.js
+++ experiment3/js/project.js
@@ -1,34 +1,123 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+/* exported preload, setup, draw, placeTile */
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
+/* global generateGrid drawGrid */
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
+let seed = 0;
+let tilesetImage;
+let currentGrid = [];
+let numRows, numCols;
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
-  }
+function preload() {
+  tilesetImage = loadImage(
+    "https://cdn.glitch.com/25101045-29e2-407a-894c-e0243cd8c7c6%2FtilesetP8.png?v=1611654020438"
+  );
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+function reseed() {
+  seed = (seed | 0) + 1109;
+  randomSeed(seed);
+  noiseSeed(seed);
+  select("#seedReport").html("seed " + seed);
+  regenerateGrid();
 }
 
-// let's get this party started - uncomment me
-//main();
+function regenerateGrid() {
+  select("#asciiBox").value(gridToString(generateGrid(numCols, numRows)));
+  reparseGrid();
+}
+
+function reparseGrid() {
+  currentGrid = stringToGrid(select("#asciiBox").value());
+}
+
+function gridToString(grid) {
+  let rows = [];
+  for (let i = 0; i < grid.length; i++) {
+    rows.push(grid[i].join(""));
+  }
+  return rows.join("\n");
+}
+
+function stringToGrid(str) {
+  let grid = [];
+  let lines = str.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    let row = [];
+    let chars = lines[i].split("");
+    for (let j = 0; j < chars.length; j++) {
+      row.push(chars[j]);
+    }
+    grid.push(row);
+  }
+  return grid;
+}
+
+function setup() {
+  numCols = select("#asciiBox").attribute("rows") | 0;
+  numRows = select("#asciiBox").attribute("cols") | 0;
+
+  createCanvas(16 * 60, 16 * 60).parent("canvasContainer");
+  select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
+
+  select("#reseedButton").mousePressed(reseed);
+  select("#asciiBox").input(reparseGrid);
+
+  reseed();
+  console.log(atob(`
+LyogZXhwb3J0ZWQgZ2VuZXJhdGVHcmlkLCBkcmF3R3JpZCAqLwovKiBnbG9iYWwg
+cGxhY2VUaWxlICovCgpmdW5jdGlvbiBnZW5lcmF0ZUdyaWQobnVtQ29scywgbnVt
+Um93cykgewogIGxldCBncmlkID0gW107CiAgZm9yIChsZXQgaSA9IDA7IGkgPCBu
+dW1Sb3dzOyBpKyspIHsKICAgIGxldCByb3cgPSBbXTsKICAgIGZvciAobGV0IGog
+PSAwOyBqIDwgbnVtQ29sczsgaisrKSB7CiAgICAgIGxldCBvdXRlclZhbHVlID0g
+bm9pc2UoaSAvIDQwLCBqIC8gNDApOwogICAgICBpZiAoYWJzKG91dGVyVmFsdWUg
+LSAwLjUpIDwgMC4wMykgewogICAgICAgIHJvdy5wdXNoKCJ3Iik7CiAgICAgIH0g
+ZWxzZSB7CiAgICAgICAgbGV0IGlubmVyVmFsdWUgPSBub2lzZShpIC8gMjAsIGog
+LyAyMCk7CiAgICAgICAgaWYgKGlubmVyVmFsdWUgPiAwLjUpIHsKICAgICAgICAg
+IHJvdy5wdXNoKCI6Iik7CiAgICAgICAgfSBlbHNlIHsKICAgICAgICAgIHJvdy5w
+dXNoKCIuIik7CiAgICAgICAgfQogICAgICB9CiAgICB9CiAgICBncmlkLnB1c2go
+cm93KTsKICB9CgogIHJldHVybiBncmlkOwp9CgpmdW5jdGlvbiBkcmF3R3JpZChn
+cmlkKSB7CgogIGJhY2tncm91bmQoMTI4KTsKICBjb25zdCBnID0gMTA7CiAgY29u
+c3QgdCA9IG1pbGxpcygpIC8gMTAwMC4wOwoKICBub1N0cm9rZSgpOwogIGZvciAo
+bGV0IGkgPSAwOyBpIDwgZ3JpZC5sZW5ndGg7IGkrKykgewogICAgZm9yIChsZXQg
+aiA9IDA7IGogPCBncmlkW2ldLmxlbmd0aDsgaisrKSB7CiAgICAgIHBsYWNlVGls
+ZShpLCBqLCAoNCAqIHBvdyhub2lzZSh0IC8gMTAsIGksIGogLyA0ICsgdCksIDIp
+KSB8IDAsIDE0KTsKCiAgICAgIGlmIChncmlkQ2hlY2soZ3JpZCwgaSwgaiwgIjoi
+KSkgewogICAgICAgIHBsYWNlVGlsZShpLCBqLCAoNCAqIHBvdyhyYW5kb20oKSwg
+ZykpIHwgMCwgMyk7CiAgICAgIH0gZWxzZSB7CiAgICAgICAgZHJhd0NvbnRleHQo
+Z3JpZCwgaSwgaiwgInciLCA5LCAzLCB0cnVlKTsKICAgICAgfQoKICAgICAgaWYg
+KGdyaWRDaGVjayhncmlkLCBpLCBqLCAiLiIpKSB7CiAgICAgICAgcGxhY2VUaWxl
+KGksIGosICg0ICogcG93KHJhbmRvbSgpLCBnKSkgfCAwLCAwKTsKICAgICAgfSBl
+bHNlIHsKICAgICAgICBkcmF3Q29udGV4dChncmlkLCBpLCBqLCAiLiIsIDQsIDAp
+OwogICAgICB9CiAgICB9CiAgfQp9CgpmdW5jdGlvbiBkcmF3Q29udGV4dChncmlk
+LCBpLCBqLCB0YXJnZXQsIGR0aSwgZHRqLCBpbnZlcnQgPSBmYWxzZSkgewogIGxl
+dCBjb2RlID0gZ3JpZENvZGUoZ3JpZCwgaSwgaiwgdGFyZ2V0KTsKICBpZiAoaW52
+ZXJ0KSB7CiAgICBjb2RlID0gfmNvZGUgJiAweGY7CiAgfQogIGxldCBbdGksdGpd
+ID0gbG9va3VwW2NvZGVdOwogIHBsYWNlVGlsZShpLCBqLCBkdGkgKyB0aSwgZHRq
+ICsgdGopOwp9CgoKCmZ1bmN0aW9uIGdyaWRDb2RlKGdyaWQsIGksIGosIHRhcmdl
+dCkgewogIHJldHVybiAoCiAgICAoZ3JpZENoZWNrKGdyaWQsIGkgLSAxLCBqLCB0
+YXJnZXQpIDw8IDApICsKICAgIChncmlkQ2hlY2soZ3JpZCwgaSwgaiAtIDEsIHRh
+cmdldCkgPDwgMSkgKwogICAgKGdyaWRDaGVjayhncmlkLCBpLCBqICsgMSwgdGFy
+Z2V0KSA8PCAyKSArCiAgICAoZ3JpZENoZWNrKGdyaWQsIGkgKyAxLCBqLCB0YXJn
+ZXQpIDw8IDMpCiAgKTsKfQoKZnVuY3Rpb24gZ3JpZENoZWNrKGdyaWQsIGksIGos
+IHRhcmdldCkgewogIGlmIChpID49IDAgJiYgaSA8IGdyaWQubGVuZ3RoICYmIGog
+Pj0gMCAmJiBqIDwgZ3JpZFtpXS5sZW5ndGgpIHsKICAgIHJldHVybiBncmlkW2ld
+W2pdID09IHRhcmdldDsKICB9IGVsc2UgewogICAgcmV0dXJuIGZhbHNlOwogIH0K
+fQoKY29uc3QgbG9va3VwID0gWwogIFsxLCAxXSwKICBbMSwgMF0sIC8vIGJvdHRv
+bQogIFswLCAxXSwgLy8gcmlnaHQKICBbMCwgMF0sIC8vIHJpZ2h0K2JvdHRvbQog
+IFsyLCAxXSwgLy8gbGVmdAogIFsyLCAwXSwgLy8gbGVmdCtib3R0b20KICBbMSwg
+MV0sCiAgWzEsIDBdLCAvLyAqIAogIFsxLCAyXSwgLy8gdG9wCiAgWzEsIDFdLAog
+IFswLCAyXSwgLy8gcmlnaHQrdG9wCiAgWzAsIDFdLCAvLyAqCiAgWzIsIDJdLCAv
+LyB0b3ArbGVmdAogIFsyLCAxXSwgLy8gKgogIFsxLCAyXSwgLy8gKgogIFsxLCAx
+XQpdOw==
+`))
+}
+
+
+function draw() {
+  randomSeed(seed);
+  drawGrid(currentGrid);
+}
+
+function placeTile(i, j, ti, tj) {
+  image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
+}
--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,79 +1,94 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+/* exported generateGrid, drawGrid */
+/* global placeTile */
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
+const lookup = [[1, 0],[1, 0], [0, 0],[0, 0],[2, 0],[2, 0],[1, 0],[1, 0],[1, 0],[1, 0],[0, 0],[0, 0],[2, 0],[2, 0],[1, 0],[1, 0]];
+const styles = [['0','1','13'],['18','19','14'],['6','7','9']];
+let rowsC = 0;
+function generateGrid(numCols, numRows) {
+  rowsC = numRows
+  let grid = [];
+  let numGrids = floor(random(2,4));
+  for (let i = 0; i < numRows*numGrids; i++) {
+    let row = [];
+    for (let j = 0; j < numCols*numGrids; j++) {
+      let corner1 = noise(i / 30, j / 30);
+      let corner2 = noise(i / 20, j / 20);
+      if(j == 0 ||  i == 0 ){
+        row.push("_");
+      }
+      else if((corner1 - 0.6) < 0.03 && (corner2 >0.5)){
+        row.push("w");
+      }
+      else{
+        row.push(".")
+      }
     }
+    grid.push(row);
+  }
+  
+  return grid;
+}
 
-    myMethod() {
-        // code to run when method is called
+function drawGrid(grid) {
+  background(255,255,255);
+  let currStyle = floor(random(0,3));
+  for(let i = 0; i < grid.length; i++) {
+    if(i%rowsC==0){
+      currStyle = floor(random(0,3))
     }
+    for(let j = 0; j < grid[i].length; j++) {
+
+      if (gridCheck(grid,i,j,'_')) {
+        placeTile(i, j, (floor(random(4))), styles[currStyle][1]);
+      }
+      else{
+        drawContext(grid, i, j, "_", 4, styles[currStyle][1]);
+      }
+      if (gridCheck(grid,i,j,'.')) {
+        placeTile(i, j, (floor(random(4))), styles[currStyle][0]);
+      }
+      else{
+        drawContext(grid, i, j, ".", 4, styles[currStyle][0]);
+      }
+      if(gridCheck(grid,i,j,'w')){
+        placeTile(i, j, (4 * pow(noise(millis()/1000 / 10, i, j  + millis()/1000), 2)) , styles[currStyle][2]);
+      }
+      else{
+        drawContext(grid, i, j, "w", 4, styles[currStyle][2]);
+      }
+    }
+  }
 }
 
-function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
+function gridCheck(grid, i, j, target) {
+  if(i < grid.length && i >= 0 && j>=0 && j < grid[i].length){
+    if(grid[i][j] == target){
+      return true;
+    }
+  }
+  return false;
 }
 
-// setup() function is called once when the program starts
-function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
-
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
+function gridCode(grid, i, j, target) {
+  //let northBit,southBit,eastBit,westBit;
+  let code = 0;
+  if(gridCheck(grid,i,j+1,target)){
+    code += 1;
+  }
+  if(gridCheck(grid,i,j-1,target)){
+    code += 1<<1;
+  }
+  if(gridCheck(grid,i-1,j,target)){
+    code += 1<<2;
+  }
+  if(gridCheck(grid,i+1,j,target)){
+    code += 1<<3;   
+  }
+  return code;
 }
 
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function drawContext(grid,i,j,target,ti,tj){
+  const [tiOffset, tjOffset] = lookup[gridCode(grid,i,j,target)];
+  placeTile(i, j, ti + tiOffset, tj + tjOffset);
+  
 }
