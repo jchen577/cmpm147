adf1e9c72a8a1ee0c950f178ba567234bb69e507
jchen577 <95502719+jchen577@users.noreply.github.com>
2024-04-23 20:08:07 -0700

    Dungeon biome addition

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,8 +1,8 @@
 /* exported generateGrid, drawGrid */
 /* global placeTile */
 
-const lookup = [[1, 0],[1, 0], [0, 0],[0, 0],[2, 0],[2, 0],[1, 0],[1, 0],[1, 0],[1, 0],[0, 0],[0, 0],[2, 0],[2, 0],[1, 0],[1, 0]];
-const styles = [['0','1','13'],['18','19','14'],['6','7','9']];
+const lookup = [[1, 2],[1, 1],[0, 1],[0, 0],[2, 2],[2, 0],[1, 1],[1, 2],[1, 0],[0, 1],[0, 1],[2, 1],[2, 0],[1, 2],[0, 2],[1,1],];
+const styles = [['0','1','13'],['18','19','14'],['6','7','9'],['23','23','15','28'],];
 let rowsC = 0;
 function generateGrid(numCols, numRows) {
   rowsC = numRows
@@ -13,7 +13,7 @@ function generateGrid(numCols, numRows) {
     for (let j = 0; j < numCols*numGrids; j++) {
       let corner1 = noise(i / 30, j / 30);
       let corner2 = noise(i / 20, j / 20);
-      if(j == 0 ||  i == 0 ){
+      if(j == 0 ||  i == 0  || j == numCols*numGrids -1 || i == numRows -1){
         row.push("_");
       }
       else if((corner1 - 0.6) < 0.03 && (corner2 >0.5)){
@@ -31,10 +31,10 @@ function generateGrid(numCols, numRows) {
 
 function drawGrid(grid) {
   background(255,255,255);
-  let currStyle = floor(random(0,3));
+  let currStyle = floor(random(0,4));
   for(let i = 0; i < grid.length; i++) {
     if(i%rowsC==0){
-      currStyle = floor(random(0,3))
+      currStyle = floor(random(0,4))
     }
     for(let j = 0; j < grid[i].length; j++) {
 
@@ -51,7 +51,18 @@ function drawGrid(grid) {
         drawContext(grid, i, j, ".", 4, styles[currStyle][0]);
       }
       if(gridCheck(grid,i,j,'w')){
-        placeTile(i, j, (4 * pow(noise(millis()/1000 / 10, i, j  + millis()/1000), 2)) , styles[currStyle][2]);
+        let chest =floor(random(0,100));
+        if(currStyle == 3){
+          if(chest==99){
+            placeTile(i, j, (floor(random(4))), styles[currStyle][3]);
+          }
+          else{
+            placeTile(i, j, (floor(random(4))), styles[currStyle][2]);
+          }
+        }
+        else{
+          placeTile(i, j, (4 * pow(noise(millis()/1000 / 10, i, j  + millis()/1000), 2)) , styles[currStyle][2]);
+        }
       }
       else{
         drawContext(grid, i, j, "w", 4, styles[currStyle][2]);
@@ -73,22 +84,22 @@ function gridCode(grid, i, j, target) {
   //let northBit,southBit,eastBit,westBit;
   let code = 0;
   if(gridCheck(grid,i,j+1,target)){
-    code += 1;
+    code = code +(1<< 0);
   }
   if(gridCheck(grid,i,j-1,target)){
-    code += 1<<1;
+    code = code +(1<< 1);
   }
   if(gridCheck(grid,i-1,j,target)){
-    code += 1<<2;
+    code = code +(1<< 2);
   }
   if(gridCheck(grid,i+1,j,target)){
-    code += 1<<3;   
+    code = code +(1<< 3); 
   }
   return code;
 }
 
 function drawContext(grid,i,j,target,ti,tj){
-  const [tiOffset, tjOffset] = lookup[gridCode(grid,i,j,target)];
+  const [tiOffset, tjOffset] = lookup[gridCode(grid, i, j, target)];
   placeTile(i, j, ti + tiOffset, tj + tjOffset);
   
 }
